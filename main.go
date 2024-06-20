77930fdd766def9fbb2912145b635d350edb5912
Christopher <christophermcd0903@gmail.com>
2024-06-20 17:08:18 -0400

    http server test

--- main.go
+++ main.go
@@ -6,25 +6,34 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
-	"os"
+	"net/http"
 
 	"golang.org/x/crypto/argon2"
 )
 
-func main() {
-	fmt.Println("Hello world")
-
-	b := argon2.IDKey([]byte(""), []byte("po-key-mon-salt"), 64, 128*1024, 8, 32)
+func get_key(input []byte) string {
+	b := argon2.IDKey(input, []byte("po-key-mon-salt"), 64, 128*1024, 8, 32)
 
 	r := bytes.NewReader(b[:])
-	pubkey, privkey, err := ed25519.GenerateKey(r)
+	_, privkey, err := ed25519.GenerateKey(r)
 	if err != nil {
 		log.Fatal("unable to generate key: ", err)
 	}
-	fmt.Println(len(pubkey))
-	fmt.Println(len(privkey))
 
-	encoder := base64.NewEncoder(base64.StdEncoding, os.Stdout)
+	var output_buffer bytes.Buffer
+	encoder := base64.NewEncoder(base64.StdEncoding, &output_buffer)
 	encoder.Write(privkey)
 	encoder.Close()
+	return output_buffer.String()
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
+
+func main() {
+	fmt.Println(get_key([]byte("hello world")))
+
+	http.HandleFunc("/test", testHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
